c56b94ea52b64ee69f251ea4d41021c56fa150f6
Ajesh Shah <ajeshshah@gmail.com>
2014-12-15 12:04:26 -0800

    dbconfig and test for autocomplete

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express"),
+  bodyParser = require("body-parser"),
+  methodOverride = require("method-override"),
+  pg = require("pg"),
+  app = express();
+
+app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
+app.get("/", function (req, res) {
+  res.render("game/index");
+});
+
+
+
+
+app.listen(3000, function () {
+  console.log(new Array(51).join("*"));
+  console.log("\t LISTENING ON: \n\t\t localhost:3000");
+  console.log(new Array(51).join("*")); 
+});
